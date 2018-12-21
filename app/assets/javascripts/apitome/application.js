763afa8b57ea63f43548dfcf93bbc346e2b98582
Anna Molly <akhsunna4@gmail.com>
2018-12-21 18:11:34 +0200

    add timeout to fix wrong offsets

--- app/assets/javascripts/apitome/application.js
+++ app/assets/javascripts/apitome/application.js
@@ -5,13 +5,20 @@
 hljs.initHighlightingOnLoad();
 
 jQuery(function(){
-  $(document.body).scrollspy({target: '.sidebar', offset: $('.navbar').outerHeight(true)});
-  var $sideBar = $('.single-page .sidebar');
-  $sideBar.affix({
-    offset: {
-      top: function () { return this.top = $sideBar.offset().top - $('.docs-nav').height() - 30 }
-    }
-  })
+  setTimeout(function(){
+    $(document.body).scrollspy({
+      target: '.sidebar',
+      offset: $('.navbar').outerHeight(true)
+    });
+    var $sideBar = $('.single-page .sidebar');
+    $sideBar.affix({
+      offset: {
+        top: function () {
+          return this.top = $sideBar.offset().top - $('.docs-nav').height() - 30
+        }
+      }
+    });
+  }, 1000);
 });
 
 $(document).ready(function() {
